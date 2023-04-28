cccbcb5a259198c0928424705bd5ebd6fa7f2787
QuinielaMaster <132105234+QuinielaMaster@users.noreply.github.com>
2023-04-28 15:31:24 -0600

    Delete aleatorio.js

--- aleatorio.js
+++
@@ -1,9 +0,0 @@
-function generarMarcadores() {
-  // Obtener todos los campos de goles
-  const goles = document.querySelectorAll('input[type="number"]');
-
-  // Generar números aleatorios entre 0 y 3 y asignarlos a los campos de goles
-  goles.forEach((gol) => {
-    gol.value = Math.floor(Math.random() * 4);
-  });
-}
